17544774ec2f95844b07309ee90fae3fa9dd80e4
davirosarback <82427024+davirosarback@users.noreply.github.com>
2022-11-01 17:44:48 -0300

    novo

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function showHide(){
     const dropDown = document.getElementById('login-web')
     dropDown.classList.toggle('show')
 }
+
+function toggleMenu() {
+    const menu = document.getElementById('menu').classList
+    menu.toggle('show-menu')
+}
